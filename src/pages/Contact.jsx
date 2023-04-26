c1b8114ba124b0f27d31e4165b8f86ee95f00248
camila garcia <milaca1991@hotmail.com>
2023-04-26 13:32:51 +0200

    first commit

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,6 @@
+function Books(){
+
+    return <h1>Books</h1>
+}
+
+export default Books;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+function Home(){
+
+    return (
+    
+        <div className="home">
+    
+        <h1>Home</h1>
+    
+    
+    
+        </div>
+    
+    
+    )
+}
+    
+export default Home;
